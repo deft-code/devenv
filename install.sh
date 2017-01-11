211dffdfb3aa5cc29f3a98f193944d91f611405d
Nick Terry <j.nick.terry@gmail.com>
2017-01-11 15:46:21 -0800

    Clean up some more submodules.

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ rm -rf .vim .vimrc devenv
 git clone --recursive https://github.com/deft-code/devenv.git devenv
 ln -s devenv/vim .vim
 ln -s devenv/vimrc .vimrc
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
